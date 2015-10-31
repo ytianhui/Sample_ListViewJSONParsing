78c95618a8d4cd58503e7b2008eb22fb22760c94
ytianhui2004@gmail.com <yth220517>
2015-10-31 16:14:35 +0900

    Initial commit

---
+++ app/build/generated/source/buildConfig/debug/learn2crack/listview/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package learn2crack.listview;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "learn2crack.listview";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "";
+}
